49863e430f7acb047cd9438ba9473483d12b7f70
Mathieu Leplatre <mathieu@mozilla.com>
2017-04-12 12:27:05 +0200

    Upgrade tests to Kinto master

--- kinto_emailer/tests/test_includeme.py
+++ kinto_emailer/tests/test_includeme.py
@@ -39,8 +39,9 @@ class EmailerTest(BaseWebTest, unittest.TestCase):
     api_prefix = "v1"
     config = 'config/kinto.ini'
 
-    def get_app_settings(self, extras=None):
-        ini_path = os.path.join(HERE, self.config)
+    @classmethod
+    def get_app_settings(cls, extras=None):
+        ini_path = os.path.join(HERE, cls.config)
         config = configparser.ConfigParser()
         config.read(ini_path)
         settings = dict(config.items('app:main'))
@@ -325,8 +326,9 @@ class SendNotificationTest(unittest.TestCase):
 
 
 class SignerEventsTest(EmailerTest):
-    def get_app_settings(self, extras=None):
-        settings = super(SignerEventsTest, self).get_app_settings(extras)
+    @classmethod
+    def get_app_settings(cls, extras=None):
+        settings = super().get_app_settings(extras)
         settings['kinto.includes'] += ' kinto_signer'
         settings['kinto.signer.resources'] = (
             '/buckets/staging/collections/addons;'
