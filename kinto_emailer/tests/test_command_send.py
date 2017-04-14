47e7d32b63ef8eaf4e6c9be5e858ed169ea8c52a
Rémy HUBSCHER <rhubscher@mozilla.com>
2017-04-14 10:36:50 +0200

    Fix import.

--- kinto_emailer/tests/test_command_send.py
+++ kinto_emailer/tests/test_command_send.py
@@ -1,20 +1,20 @@
 import mock
 import unittest
 
-from kinto_emailer import send_email
+from kinto_emailer import command_send
 
 
 class CommandTest(unittest.TestCase):
     def test_uses_sys_args_by_default(self):
-        assert send_email.main() > 0  # will fail
+        assert command_send.main() > 0  # will fail
 
     def test_returns_non_zero_if_not_enough_args(self):
-        assert send_email.main([]) > 0
+        assert command_send.main([]) > 0
 
     def test_calls_send_immmediately_with_parameters(self):
-        with mock.patch('kinto_emailer.send_email.bootstrap') as bootstrap:
-            with mock.patch('kinto_emailer.send_email.get_mailer') as get_mailer:
-                send_email.main(["config.ini", "me@restmail.net"])
+        with mock.patch('kinto_emailer.command_send.bootstrap'):
+            with mock.patch('kinto_emailer.command_send.get_mailer') as get_mailer:
+                command_send.main(["config.ini", "me@restmail.net"])
 
                 args, kwargs = get_mailer().send_immediately.call_args_list[0]
                 assert "kinto-emailer" in args[0].subject
