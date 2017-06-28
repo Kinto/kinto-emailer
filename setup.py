40914b678d86e818a3e0c1003b0397949775f209
Rémy HUBSCHER <rhubscher@mozilla.com>
2017-06-28 12:19:58 -0700

    Back to development: 1.1.0

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ ENTRY_POINTS = {
 }
 
 setup(name='kinto-emailer',
-      version='1.0.0',
+      version='1.1.0.dev0',
       description='Kinto emailer plugin',
       long_description=README + "\n\n" + CHANGELOG + "\n\n" + CONTRIBUTORS,
       license='Apache License (2.0)',
