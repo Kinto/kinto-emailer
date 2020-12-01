c2f256ce96e97598a2c22fc99632ebb9b9bc5283
Mathieu Leplatre <mathieu@mozilla.com>
2020-12-01 12:28:41 +0100

    Back to development: 2.1.0

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ ENTRY_POINTS = {
 }
 
 setup(name='kinto-emailer',
-      version='2.0.0',
+      version='2.1.0.dev0',
       description='Kinto emailer plugin',
       long_description=README + "\n\n" + CHANGELOG + "\n\n" + CONTRIBUTORS,
       license='Apache License (2.0)',
